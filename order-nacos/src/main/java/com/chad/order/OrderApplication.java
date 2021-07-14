93d6cfb9166821c97c7b9ab5db30d053bfff1bba
Formerscholar <2319315514@qq.com>
2021-07-14 16:38:10 +0800

    nacos 配置+初使用+负载均很调用

---
+++ order-nacos/src/main/java/com/chad/order/OrderApplication.java
@@ -0,0 +1,28 @@
+package com.chad.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @ClassName: OrderApplication
+ * @Description: application
+ * @author: chad
+ * @date: 2021/7/13/013 20:58
+ */
+@SpringBootApplication
+public class OrderApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		RestTemplate restTemplate = builder.build();
+		return restTemplate;
+	}
+}
---
+++ stock-nacos/src/main/java/com/chad/stock/controller/StockController.java
@@ -0,0 +1,26 @@
+package com.chad.stock.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: StockController
+ * @Description: StockController
+ * @author: chad
+ * @date: 2021/7/13/013 20:56
+ */
+@RestController
+@RequestMapping("/stock")
+public class StockController {
+	
+	@Value("${server.port}")
+	String prot;
+	
+	@GetMapping("/reduct")
+	public String reduct() {
+		System.out.println("扣减库存!");
+		return "扣减库存:"+prot;
+	}
+}
